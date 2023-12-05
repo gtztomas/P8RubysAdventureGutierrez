ab377211fdb368b8a6f2aaa74fca4fc21eb65b0d
Tomas Gutierrez <723369@student.dusd.net>
2023-12-05 14:20:00 -0800

    Finished Collectables
    finished collectables script

--- RubysAdventureGutierrez/Assets/Art Samples/Scripts/RubyController.cs
+++ RubysAdventureGutierrez/Assets/Art Samples/Scripts/RubyController.cs
@@ -6,7 +6,10 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth;
+    
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -16,7 +19,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+        
     }
 
     // Update is called once per frame
